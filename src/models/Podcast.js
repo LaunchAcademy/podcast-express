8cb7fec372c7fba373bda0209f8d86f2c229cbc9
Stephen Fang <safang99@gmail.com>
2020-12-17 10:56:20 -0500

    resolved requested changes

--- src/models/Podcast.js
+++ src/models/Podcast.js
@@ -2,10 +2,6 @@ import fs from "fs"
 
 const podcastsPath = "podcasts.json"
 
-const podcastsJSON = () => {
-  return JSON.parse(fs.readFileSync(podcastsPath))
-}
-
 class Podcast {}
 
 export default Podcast
